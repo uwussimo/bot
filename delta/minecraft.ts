553da9306c267026cdef766ec68277bc5f483143
UwUssimo Robinson <uwussimo@icloud.com>
2022-06-26 19:32:18 +0500

    Update minecraft.ts

--- delta/minecraft.ts
+++ delta/minecraft.ts
@@ -1,18 +1,7 @@
-import type {Response as Minecraft, User} from "../types/minecraft.d.ts";
+import type { Response as Minecraft } from "../types/minecraft.d.ts";
 import { Composer, Context, InlineKeyboard, InputFile } from "../deps.ts";
 
 const composer = new Composer();
-
-const listPlayers = (data: Minecraft): string => {
-    let base = ""
-    if (data.content.players.sample) {
-        data.content.players.sample.map(u => {
-            base += `* ${u.name}\n`
-        })
-    } else {
-        return ""
-    }
-}
 
 export const message = (data: Minecraft): string =>
   `<b>Stackoverflow Stats!</b>` +
@@ -26,17 +15,16 @@ export const message = (data: Minecraft): string =>
   `\n` +
   `<b>➿ Software:</b> Vanilla ${data.content.version.name} => ${data.content.version.protocol}` +
   `\n` +
-  `<b>📝 Message:</b> <code>${data.content.motd.clean}</code>` + `\n` + `\n` + listPlayers(data);
+  `<b>📝 Message:</b> <code>${data.content.motd.clean}</code>`;
 
 export const keyboard = () =>
   new InlineKeyboard()
-    .text("Refresh", "mc").row()
     .url("Website", `https://uwussi.moe/minecraft`).url(
       "Repository",
       `https://github.com/uwussimo/minecraft`,
     );
 
-composer.command("mc", async (ctx: Context): Promise<void> => {
+composer.command("mc", async (ctx: Context) => {
   try {
     await fetch("https://uwussi.moe/api/minecraft").then(
       async (r: Response) => {
@@ -66,41 +54,6 @@ composer.command("mc", async (ctx: Context): Promise<void> => {
       "<b>Woah, seems like I'm facing some issues 😢.</b>" + "\n" +
         "I don't remember myself installing php, python or apache in my server 🧐",
       {
-        parse_mode: "HTML",
-      },
-    );
-  }
-});
-
-composer.callbackQuery(/^mc$/, async (ctx: Context): Promise<void> => {
-  try {
-    await fetch("https://uwussi.moe/api/minecraft").then(
-      async (r: Response) => {
-        const json: Minecraft = await r.json();
-
-        if (json.status) {
-          await ctx.editMessageCaption(
-            {
-              caption: message(json),
-              parse_mode: "HTML",
-              reply_markup: keyboard(),
-            },
-          );
-        } else {
-          await ctx.reply(
-            "<b>Woah, seems like server went offline 😢.</b>",
-            {
-              parse_mode: "HTML",
-            },
-          );
-        }
-      },
-    );
-  } catch (_) {
-    await ctx.editMessageCaption(
-      {
-        caption: "<b>Woah, seems like I'm facing some issues 😢.</b>" + "\n" +
-          "I don't remember myself installing php, python or apache in my server 🧐",
         parse_mode: "HTML",
       },
     );
