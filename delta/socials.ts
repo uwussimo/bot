ee40e9ceeaae84c402c9c1e497d06704516addac
UwUssimo Robinson <uwussimo@icloud.com>
2022-07-03 00:18:14 +0500

    Update socials.ts

--- delta/socials.ts
+++ delta/socials.ts
@@ -8,11 +8,11 @@ export const message: string = `<b>Welcome to my assistant bot!</b> \n` +
   `Also, this bot will serve for me as a shortcut and automation.`;
 
 export const keyboard = new InlineKeyboard()
-  .url("GitHub", "https://github.com/uwussimo")
-  .url("Telegram", "https://t.me/uwublog")
-  .url("Discord", "https://discord.gg/JkXFQpScFj")
-  .url("Twitter", "https://twitter.com/uwussimo")
-  .url("Keybase", "https://keybase.io/uwussimo");
+  .url("⚫️ GitHub", "https://github.com/uwussimo")
+  .url("🔵 Telegram", "https://t.me/uwublog")
+  .url("🟣 Discord", "https://discord.gg/JkXFQpScFj")
+  .url("🔵 Twitter", "https://twitter.com/uwussimo")
+  .url("🟠 Keybase", "https://keybase.io/uwussimo");
 
 composer.command("start", async (ctx: Context): Promise<void> => {
   await ctx.reply(message, {
